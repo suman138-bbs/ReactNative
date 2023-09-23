c2c379aad042a68847d52a5a9fd33e1872b24eac
Suman Bhandari <sumanbhandari9860@gmail.com>
2023-09-23 21:52:53 +0530

    Input and Title

--- Code/LProject/App.js
+++ Code/LProject/App.js
@@ -1,13 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { useState } from 'react';
-import {Focus}  from './src/features/focus/Focus';
+
 export default function App() {
-  const [focusSubject,setFocusSubject] = useState("Suman")
   return (
     <View style={styles.container}>
-      <Text>{focusSubject} </Text>
-      <Focus/>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,10 +12,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    padding:50,
     flex: 1,
-    backgroundColor: '#252250',
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
